3dc076d7b6f32a245610fb8f23688e4beaa6ce7f
Sikanto Mandal <mandalsikanto@gmail.com>
2024-06-01 22:51:29 +0530

    pattern

--- Pattern.java
+++ Pattern.java
@@ -1,12 +1,17 @@
 public class Pattern {
     public static void printPattern(int n){       
         for(int i =1; i<=n; i++){
+
+            // left side numbers
             for(int j =1; j<=i; j++){
                 System.out.print(j);                
-            }      
+            }   
+            //space   
             for(int k = 1; k<=2*(n-i)-1; k++){
                 System.out.print(" ");
             }
+
+            //right side numbers
             for(int j =i; j>=1; j--){
                 if(j !=n)
                   System.out.print(j);                
